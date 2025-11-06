e408284a0147c6a18b3e9eb3169a5baec394da36
svnzzz <fghghhn214@gmail.com>
2025-11-06 15:27:49 +0100

    init repo

---
+++ vehicle/vehicle.entity.go
@@ -0,0 +1,16 @@
+package vehicle
+
+type Vehicle struct {
+	Model           string  `json:"Modello"`
+	BasePrice       float32 `json:"PrezzoBase"`
+	BackgroundImage string  `json:"FileImageSfondo"`
+}
+
+type Optional struct {
+	ModelID   int     `json:"ModelloID"`
+	Name      string  `json:"Optional"`
+	Model     string  `json:"Modello"`
+	Price     float32 `json:"Prezzo"`
+	FileImage string  `json:"FileImage"`
+	Default   bool    `json:"Predefinito"`
+}
